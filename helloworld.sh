dfbc75b40b86554edb9e33ccb0811a237001436e
lydiaxnguyen <lydiaxnguyen@gmail.com>
2017-07-13 12:59:16 -0400

    first scripting

---
+++ helloworld.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "This is what directory you are in: `pwd`"
+echo ""
+echo "These are what files are there: `.`"
+echo ""
+echo "This is the date today `date --iso-8601`"
+echo ""
+echo "This is the system name and IP address: `hostname`"
